8faababb5180b043f3d83244c26dee6149798a11
afrahrasheed <afrarasheed003@gmail.com>
2026-07-08 11:33:06 +0400

    Create SystemReliabilityException.java

---
+++ Part1_DroneNavigation/Part1_DroneNavigation/SystemReliabilityException.java
@@ -0,0 +1,12 @@
+package drone;
+
+
+public class SystemReliabilityException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+   
+    public SystemReliabilityException(String message) {
+        super(message);
+    }
+}
